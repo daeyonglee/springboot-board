49d3efb73c1c4c2a07a7a5852bef55453c883f0b
DY LEE <dleo238@gmail.com>
2024-03-23 22:47:54 +0900

    up

--- board/src/main/resources/data.sql
+++ board/src/main/resources/data.sql
@@ -373,6 +373,8 @@ Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus
 Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.', null, 'Wilmer', 'Ingra', '2021-07-19 14:18:17', '2022-01-23 17:29:54')
 ;
 
+
+
 -- 1000 댓글
 insert into article_comment (article_id, content, created_at, modified_at, created_by, modified_by) values
                                                                                                         (49, 'Quisque id justo sit amet sapien dignissim vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Nulla dapibus dolor vel est. Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.', '2021-03-02 22:40:04', '2021-04-27 15:38:09', 'Lind', 'Orv'),
