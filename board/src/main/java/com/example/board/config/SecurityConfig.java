a08cce4cf1ca2869028f02cfab96f056bb69eb2b
DY LEE <dleo238@gmail.com>
2024-05-08 21:44:32 +0900

    bootstrap 설정

---
+++ board/src/main/java/com/example/board/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.example.board.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest()
+                        .permitAll()).formLogin((formLogin) -> formLogin.defaultSuccessUrl("/articles"))
+                .httpBasic(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+}
--- board/src/main/java/com/example/board/controller/ArticleController.java
+++ board/src/main/java/com/example/board/controller/ArticleController.java
@@ -1,5 +1,6 @@
 package com.example.board.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +10,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @RequestMapping("/articles")
+@Slf4j
 @Controller
 public class ArticleController {
     @GetMapping
     public String articles(ModelMap map) {
+        log.info("^^^^^^^^^");
         map.addAttribute("articles", List.of());
         return "articles/index";
     }
 
     @GetMapping("/{articleId}")
-    public String article(@PathVariable Long articleId, ModelMap map) {
-        System.out.println(articleId);
+    public String article(@PathVariable("articleId") Long articleId, ModelMap map) {
+        log.info("^^^^^^^" + articleId);
         map.addAttribute("article", "article"); // TODO: 구현할 때 여기에 실제 데이터를 넣어줘야 한다
         map.addAttribute("articleComments", List.of());
 
