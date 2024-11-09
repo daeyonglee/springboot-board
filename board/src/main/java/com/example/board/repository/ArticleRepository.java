f5d00b6c793412e0587464acb8e374a50a87b8db
DaeyongLee <dy.lee@tosky.co.kr>
2024-11-09 22:21:14 +0900

    PaginationService 구현

--- board/src/main/java/com/example/board/repository/ArticleRepository.java
+++ board/src/main/java/com/example/board/repository/ArticleRepository.java
@@ -30,5 +30,11 @@ public interface ArticleRepository extends
         bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
     }
 
+    Page<Article> findByTitleContaining(String title, Pageable pageable);
+    Page<Article> findByContentContaining(String content, Pageable pageable);
+    Page<Article> findByUserAccount_UserIdContaining(String userId, Pageable pageable);
+    Page<Article> findByUserAccount_NicknameContaining(String nickname, Pageable pageable);
+    Page<Article> findByHashtag(String hashtag, Pageable pageable);
+
     Page<Article> findByTitle(String title, Pageable pageable);
 }
--- board/src/test/java/com/example/board/controller/AuthControllerTest.java
+++ board/src/test/java/com/example/board/controller/AuthControllerTest.java
@@ -9,11 +9,12 @@ import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @DisplayName("View 컨트롤러 - 인증")
 @Import(SecurityConfig.class)
-@WebMvcTest
+@WebMvcTest(Void.class)
 public class AuthControllerTest {
 
     private final MockMvc mvc;
@@ -30,7 +31,7 @@ public class AuthControllerTest {
         // When & Then
         mvc.perform(MockMvcRequestBuilders.get("/login"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML)).andDo(MockMvcResultHandlers.print());
 
     }
 }
